fce84bf13d587e4b666b72e3aea851d53cf17028
JorgeGonSan <162482388+JorgeGonSan@users.noreply.github.com>
2026-03-23 17:53:58 +0100

    añado factory constructor desde JSON al modelo Usuario

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -10,4 +10,21 @@ class Usuario {
     required this.ciudad,
     required this.urlAvatar,
   });
+
+  factory Usuario.fromJson(Map<String, dynamic> datosjson) {
+    final firstNameApi = datosjson['name']?['first'] ?? 'Sin nombre';
+    final lastNameApi = datosjson['name']?['last'] ?? 'sin apellido';
+    final fullNameApi = "$firstNameApi $lastNameApi";
+
+    final emailApi = datosjson['email'] ?? 'Sin email';
+    final ciudadApi = datosjson['location']?['city'] ?? 'Sin definir';
+    final urlAvatarApi = datosjson['picture']?['medium'] ?? 'Sin iamgen';
+
+    return Usuario(
+      nombre: fullNameApi,
+      email: emailApi,
+      ciudad: ciudadApi,
+      urlAvatar: urlAvatarApi,
+    );
+  }
 }
