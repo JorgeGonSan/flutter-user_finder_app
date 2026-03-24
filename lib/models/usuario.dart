d6d8fdfb965ac2dd007a3502be8e2c517f80f3a4
JorgeGonSan <162482388+JorgeGonSan@users.noreply.github.com>
2026-03-24 20:08:51 +0100

    Añado nuevos atributos al modelo usuario para mostrar en pantalla de detalles usuario

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -3,28 +3,43 @@ class Usuario {
   final String email;
   final String ciudad;
   final String urlAvatar;
+  final String telefono;
+  final int edad;
+  final String genero;
+  final String pais;
 
   const Usuario({
     required this.nombre,
     required this.email,
     required this.ciudad,
     required this.urlAvatar,
+    required this.telefono,
+    required this.edad,
+    required this.genero,
+    required this.pais,
   });
 
   factory Usuario.fromJson(Map<String, dynamic> datosjson) {
     final firstNameApi = datosjson['name']?['first'] ?? 'Sin nombre';
     final lastNameApi = datosjson['name']?['last'] ?? 'sin apellido';
     final fullNameApi = "$firstNameApi $lastNameApi";
-
     final emailApi = datosjson['email'] ?? 'Sin email';
     final ciudadApi = datosjson['location']?['city'] ?? 'Sin definir';
     final urlAvatarApi = datosjson['picture']?['medium'] ?? 'Sin iamgen';
+    final telefonoApi = datosjson['phone'] ?? 'Sin definir';
+    final edadApi = datosjson['dob']?['age'] ?? 0;
+    final generoApi = datosjson['gender'] ?? "Sin definir";
+    final paisApi = datosjson['location']?['country'] ?? 'Sin definir';
 
     return Usuario(
       nombre: fullNameApi,
       email: emailApi,
       ciudad: ciudadApi,
       urlAvatar: urlAvatarApi,
+      telefono: telefonoApi,
+      edad: edadApi,
+      genero: generoApi,
+      pais: paisApi,
     );
   }
 }
