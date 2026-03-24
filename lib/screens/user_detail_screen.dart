43f7911754d86460432a86fcf4b3314ec91817f0
JorgeGonSan <162482388+JorgeGonSan@users.noreply.github.com>
2026-03-24 19:36:09 +0100

    Pantalla detalle de usuario completa con avata, nombre, ciudad y email

--- lib/screens/user_detail_screen.dart
+++ lib/screens/user_detail_screen.dart
@@ -13,6 +13,65 @@ class UserDetailScreen extends StatelessWidget {
         title: Text("User Details"),
         backgroundColor: Colors.grey[300],
       ),
+      //permite scroll
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Center(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey[300],
+                  radius: 90,
+                  child: CircleAvatar(
+                    radius: 80,
+                    backgroundImage: NetworkImage(userDetail.urlAvatar),
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                userDetail.nombre,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+            SizedBox(height: 8),
+            Divider(
+              thickness: 1,
+              color: Colors.grey[300],
+              indent: 50,
+              endIndent: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Row(
+                children: [
+                  Icon(Icons.location_city, color: Colors.grey[500]),
+                  SizedBox(width: 8),
+                  Text(userDetail.ciudad),
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Row(
+                children: [
+                  Icon(Icons.mail, color: Colors.grey[500]),
+                  SizedBox(width: 8),
+                  Text(userDetail.email, overflow: TextOverflow.ellipsis),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
