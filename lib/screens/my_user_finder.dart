2a8e9cdb4bcbaedf85fd7ac29e2567dad3a43f56
JorgeGonSan <162482388+JorgeGonSan@users.noreply.github.com>
2026-03-26 18:58:46 +0100

    añado buscador de usuarios por nombre

--- lib/screens/my_user_finder.dart
+++ lib/screens/my_user_finder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:user_finder/models/usuario.dart';
 import 'package:user_finder/services/api_service.dart';
 import 'package:user_finder/widgets/my_list_view_builder.dart';
 
@@ -12,14 +13,40 @@ class MyUserFinderScreen extends StatefulWidget {
 class _MyUserFinderScreenState extends State<MyUserFinderScreen> {
   String textBuscador = "";
   TextEditingController miController = TextEditingController();
+  //Lista con todos los usuarios para realizar busquedas
+  List<Usuario> usuariosTotales = [];
+  //Lista de solo los filtrados
+  List<Usuario> usuariosFiltrados = [];
+  bool hayError = false;
+
+  /*
+  llamamos al crear la pantalla una unica vez llamamos a la apiService.cargarUsusarios
+  que nos devolvera una lista de usuarios en el futuro, entonces (Then) cuando llegue
+  copiamos la lista que llego desde la API a la variable usuarios totales y usuarios filttado para
+  inicialmente mostrar todos los usuarios.
+  Controlamos en el caso de que haya error para mostrarlo
+  */
+  @override
+  void initState() {
+    super.initState();
+
+    ApiService.cargarUsuarios()
+        .then((listaDeLaApi) {
+          setState(() {
+            usuariosTotales = listaDeLaApi;
+            usuariosFiltrados = listaDeLaApi;
+          });
+        })
+        .catchError((miError) {
+          setState(() {
+            hayError = true;
+          });
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /*
-      futuro Builder pasamos la funcion futura y miramos que tal va, la carga
-      el snapshot tiene esa informacion
-      */
       body: Column(
         children: [
           Padding(
@@ -28,7 +55,16 @@ class _MyUserFinderScreenState extends State<MyUserFinderScreen> {
               controller: miController,
               onChanged: (value) {
                 setState(() {
-                  textBuscador = miController.text;
+                  //recorremos usuarios totales
+                  final resultado = usuariosTotales.where((usuarioBuscado) {
+                    //Por cada usuario cogemos nombre y lo pasamos a minúsculas
+                    final nombreUsuario = usuarioBuscado.nombre.toLowerCase();
+                    //pasamos el texto del texfield tambien a minúsculas
+                    textBuscador = value.toLowerCase();
+                    //comprueba si contine el texto buscado
+                    return nombreUsuario.contains(textBuscador);
+                  }).toList();
+                  usuariosFiltrados = resultado;
                 });
               },
               decoration: InputDecoration(
@@ -39,25 +75,11 @@ class _MyUserFinderScreenState extends State<MyUserFinderScreen> {
           ),
 
           Expanded(
-            child: FutureBuilder(
-              future: ApiService.cargarUsuarios(),
-              builder: (context, snapshot) {
-                //si hasError msg algo a fallado
-                if (snapshot.hasError) {
-                  return Center(child: Text("Algo a fallado"));
-                }
-                //Si no a acabado, muestro circulo de progresión
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                //Si hay datos, muestro todo cargado
-                if (snapshot.hasData) {
-                  return MyListViewBuilder(usuarios: snapshot.data);
-                }
-                //Si todo lo demas falla, muesto msg
-                return Center(child: Text("Nada a mostrar"));
-              },
-            ),
+            child: hayError
+                ? Center(child: Text("Algo a fallado"))
+                : usuariosTotales.isEmpty
+                ? Center(child: CircularProgressIndicator())
+                : MyListViewBuilder(usuarios: usuariosFiltrados),
           ),
         ],
       ),
